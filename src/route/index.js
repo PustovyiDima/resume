74a07e4f19b5ce3396e5e2bc20ba4f24d5a81858
pustovyidima <pustovoydima@gmail.com>
2023-04-10 18:20:11 +0300

    create a task 1 hmw 6

--- src/route/index.js
+++ src/route/index.js
@@ -417,6 +417,7 @@ router.get('/bio', function (req, res) {
 router.get('/program', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('program', {
+    layout: 'program',
     program: {
       excursion: {
         name: 'Cultural Tour',
