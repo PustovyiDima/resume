f8f38bc52bd7cb54d190e8a96e2d955fca9f8fad
pustovyidima <pustovoydima@gmail.com>
2023-04-11 20:13:57 +0300

    add bootstrap page 2nd lesson

--- src/route/index.js
+++ src/route/index.js
@@ -626,6 +626,14 @@ router.get('/web', function (req, res) {
   })
 })
 
+router.get('/bootstrap', function (req, res) {
+  res.render('bootstrap', {
+    layout: 'bootstrap',
+    page: {
+      tittle: 'Resume | Bootstrap',
+    },
+  })
+})
 // ================================================================
 // Підключаємо роутер до бек-енду
 module.exports = router
